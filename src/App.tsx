f48cf936e535699966d839be6bc367d999dd1e8e
Carlos Mendoza <carlosmendoza1719@gmail.com>
2024-10-18 17:38:01 -0600

    Starting project, modified welcome page that will include the login, css

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="welcome-container">
+        <div className="welcome-box">
+          <h1>Welcome to PokemonTrainer</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolorem quis itaque voluptates at debitis facere, fugiat obcaecati quisquam animi? Sapiente et modi commodi harum, rerum hic cumque voluptate nobis.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
