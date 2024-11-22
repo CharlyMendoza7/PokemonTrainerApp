90055572ea2dd4d10f6667a53b0555909adffa3e
Carlos Mendoza <carlosmendoza1719@gmail.com>
2024-11-21 19:58:06 -0600

    initialized PokemonTrainer api project

---
+++ PokemonTrainerAPI/PokemonTrainer/PokemonTrainer/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace PokemonTrainer.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public DateOnly Birth {  get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
