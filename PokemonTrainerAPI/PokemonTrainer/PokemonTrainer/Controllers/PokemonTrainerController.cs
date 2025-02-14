9fda1e7fa9015535d812cdeccf80ed58cd60c283
Carlos Mendoza <carlosmendoza1719@gmail.com>
2025-02-13 21:48:12 -0600

    added register endpoint

--- PokemonTrainerAPI/PokemonTrainer/PokemonTrainer/Controllers/PokemonTrainerController.cs
+++ PokemonTrainerAPI/PokemonTrainer/PokemonTrainer/Controllers/PokemonTrainerController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PokemonTrainer.Data;
 using PokemonTrainer.Models;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace PokemonTrainer.Controllers
 {
@@ -35,6 +39,41 @@ namespace PokemonTrainer.Controllers
             if(user == null) { return Ok(false); }
 
             return Ok(user);
+        }
+
+        [HttpPost("registerNewUser")]
+        public async Task<ActionResult<IEnumerable<User>>> InsertUser(User user)
+        {
+            if(string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.UserName) || string.IsNullOrEmpty(user.PasswordHash) || !user.Birth.HasValue)
+            {
+                return BadRequest(HttpStatusCode.BadRequest);
+            }
+
+            string hashedPassword = HashPasswordSHA256(user.PasswordHash);
+            var model = new User
+            {
+                UserName = user.UserName,
+                PasswordHash = hashedPassword,
+                Email = user.Email,
+                Birth = user.Birth,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Gender = user.Gender,
+                Permission = "user"
+            };
+
+            await _context.Users.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        private string HashPasswordSHA256(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(bytes).Replace("-", "").ToLower(); // Convert to hex string
+            }
         }
     }
 }
--- PokemonTrainerAPI/PokemonTrainer/PokemonTrainer/Models/User.cs
+++ PokemonTrainerAPI/PokemonTrainer/PokemonTrainer/Models/User.cs
@@ -6,10 +6,11 @@
         public string UserName { get; set; }
         public string FirstName {  get; set; }
         public string LastName { get; set; }
-        public DateTime Birth {  get; set; }
+        public DateTime? Birth {  get; set; }
         public string Gender { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string Permission {  get; set; }
     }
 }
