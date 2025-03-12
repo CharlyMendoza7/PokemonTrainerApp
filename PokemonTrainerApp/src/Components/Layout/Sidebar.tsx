c33264d0a152a6ac75c4c06e8d5e800f03d0a3c5
Carlos Mendoza <carlosmendoza1719@gmail.com>
2025-03-11 18:05:32 -0600

    added the side bar navigation menu, added first part of styles to the component

--- PokemonTrainerApp/src/Components/Layout/Sidebar.tsx
+++ PokemonTrainerApp/src/Components/Layout/Sidebar.tsx
@@ -1,17 +1,46 @@
 import { NavLink } from "react-router-dom"
 import pokemonLogo from "../../assets/pokemonLogo.svg"
+import { FaBook, FaBox, FaCog, FaGamepad, FaHome } from "react-icons/fa"
+import { MdCatchingPokemon } from "react-icons/md"
+import '../../styles/sidebar.css'
 
 export const Sidebar = () => {
     return (
-        <div>
+        <div className="sidebar">
             {/* Logo */}
             <NavLink to="/" className="logo">
                 <h1 className='header'>
                     <img src={pokemonLogo}
                         alt="Pokemon Logo" className='logo' />
-                    Trainer NAV
+                    Trainer
                 </h1>
             </NavLink>
+
+            {/* Navigation Menu */}
+            <nav className="menu">
+                <NavLink to="/">
+                    <FaHome /><span>Your Pokémon Team</span>
+                </NavLink>
+                <NavLink to="/storage">
+                    <FaBox /><span>Pokémon Storage</span>
+                </NavLink>
+                <NavLink to="/pokedex">
+                    <MdCatchingPokemon /><span>Pokédex</span>
+                </NavLink>
+                <NavLink to="/wiki">
+                    <FaBook /><span>Wiki</span>
+                </NavLink>
+                <NavLink to="battles">
+                    <FaGamepad /> <span>Battle Center</span>
+                </NavLink>
+            </nav>
+
+            {/* Settings at the bottom */}
+            <div>
+                <NavLink to="/settings">
+                    <FaCog /><span>Settings</span>
+                </NavLink>
+            </div>
         </div>
     )
 }
