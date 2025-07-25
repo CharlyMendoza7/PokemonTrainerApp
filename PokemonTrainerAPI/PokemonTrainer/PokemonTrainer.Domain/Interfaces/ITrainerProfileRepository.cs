a447a49c16b733f164bd67e32b0e71d506696969
Carlos Mendoza <carlosmendoza1719@gmail.com>
2025-07-24 20:00:33 -0600

    add trainerprofilerepository

---
+++ PokemonTrainerAPI/PokemonTrainer/PokemonTrainer.Domain/Interfaces/ITrainerProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using PokemonTrainer.Domain.Entities;
+
+namespace PokemonTrainer.Domain.Interfaces
+{
+    public interface ITrainerProfileRepository
+    {
+        Task AddAsync(TrainerProfile profile);
+        Task<TrainerProfile?> GetByUserIdAsync(int userId);
+        Task UpdateAsync(TrainerProfile profile);
+        Task SaveChangesAsync();
+    }
+}
---
+++ PokemonTrainerAPI/PokemonTrainer/PokemonTrainer.Infrastructure/Repositories/TrainerProfileRepository.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using PokemonTrainer.Domain.Entities;
+using PokemonTrainer.Domain.Interfaces;
+using PokemonTrainer.Infrastructure.Data;
+
+namespace PokemonTrainer.Infrastructure.Repositories
+{
+    public class TrainerProfileRepository : ITrainerProfileRepository
+    {
+        private readonly PokemonTrainerDbContext _context;
+
+        public TrainerProfileRepository(PokemonTrainerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TrainerProfile profile)
+        {
+            await _context.TrainerProfiles.AddAsync(profile);
+        }
+
+        public async Task<TrainerProfile?> GetByUserIdAsync(int userId)
+        {
+            return await _context.TrainerProfiles.FirstOrDefaultAsync(p => p.UserId == userId);
+        }
+
+        public async Task UpdateAsync(TrainerProfile profile)
+        {
+            _context.TrainerProfiles.Update(profile);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
